c94574466bc6608865ee89cf1c4894c329260cf6
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2019-03-26 14:58:59 -0400

    app search works, complete

--- index.js
+++ index.js
@@ -5,58 +5,40 @@
 let userNameInput = "";
 
 
-function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
-}
-
-function displayResults(responseJson, maxResults) {
+function displayResults(responseJson) {
   // if there are previous results, remove them
   console.log(responseJson);
   $('#results-list').empty();
-  // iterate through the articles array, stopping at the max number of results
-  for (let i = 0; i < responseJson.articles.length & i<maxResults ; i++){
+
+  for (let i = 0; i < responseJson.length ; i++){
     // for each video object in the articles
     //array, add a list item to the results 
     //list with the article title, source, author,
     //description, and image
     $('#results-list').append(
-      `<li><h3><a href="${responseJson.articles[i].url}">${responseJson.articles[i].title}</a></h3>
-      <p>${responseJson.articles[i].source.name}</p>
-      <p>By ${responseJson.articles[i].author}</p>
-      <p>${responseJson.articles[i].description}</p>
-      <img src='${responseJson.articles[i].urlToImage}'>
+      `<li><h3><a href="${responseJson[i].html_url}">${responseJson[i].name}</a></h3>
+      <p>${responseJson[i].name}</p>
+      <p>${responseJson[i].description}</p>
       </li>`
     )};
   //display the results section  
   $('#results').removeClass('hidden');
 };
 
-function getRepos(query, maxResults=10) {
-  const params = {
-    q: query,
-    language: "en",
-  };
-  const queryString = formatQueryParams(params)
-  const url = `https://api.github.com/${userNameInput}/hotdogmcgee/repos`
+function getRepos(searchTerm) {
+  const url = `https://api.github.com/users/${searchTerm}/repos`;
   //const url = searchURL + '?' + queryString;
 
   console.log(url);
 
-  const options = {
-    headers: new Headers({
-      "X-Api-Key": apiKey})
-  };
-
-  fetch(url, options)
+  fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => displayResults(responseJson, maxResults))
+    .then(responseJson => displayResults(responseJson))
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
@@ -66,8 +48,7 @@ function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     const searchTerm = $('#js-search-term').val();
-    const maxResults = $('#js-max-results').val();
-    getRepos(searchTerm, maxResults);
+    getRepos(searchTerm);
   });
 }
 
